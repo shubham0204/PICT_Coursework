39c5e9e518c27a5febaf539fb812155c93eecf3e
shubham0204 <equipintelligence@gmail.com>
2022-10-04 18:40:09 +0530

    updated assignment 2

--- 02/assignment_2.cpp
+++ 02/assignment_2.cpp
@@ -62,13 +62,12 @@ public:
 	// Creating Student object dynamically with a static method
 	static Student* createStudentEntry() {
 		Student* newStudent = new( Student ) ;
-
 		cout << "Enter student name : " << endl ;
 		string name_ ;
 		cin >> name_ ;
 		cout << name_ << endl ;
 		newStudent -> name = name_ ;
-
+        newStudent -> studentRollNumber = 89 ;
 		return newStudent ;
 	}
 
@@ -76,7 +75,6 @@ public:
 		delete deleteStudent ;
 	}
 
-	friend void print( Student& student ) ;
 	friend class Database ;
 
 };
@@ -101,7 +99,11 @@ public:
 		return database[ index ] ;
 	}
 
-	void print( ) {
+    void updateName( Student* student , string newName ) {
+        student -> name = newName ;
+    }
+
+	void print() {
 	    cout << "----------------------" << endl ;
 		for( Student* student : database ) {
 			cout << "Name : " << student -> name << endl ;
@@ -112,12 +114,6 @@ public:
 
 };
 
-void print( Student& student ) {
-	cout << "Name : " << student.name << endl ;
-	cout << "Roll number : " << student.studentRollNumber << endl ;
-}
-
-
 int Student::count = 0 ;
 
 int main() {
@@ -127,7 +123,8 @@ int main() {
 	while ( true ) {
 		cout << " Choose an operation : " << endl ; 
 		cout << "1 -> Enter student data " << endl ; 
-		cout << "2 -> Delete student " << endl ; 
+		cout << "2 -> Delete student " << endl ;
+        cout << "3 -> Update" << endl ;
 		cout << "0 -> Exit " << endl ; 
 		int option ; 
 		try {
@@ -144,15 +141,32 @@ int main() {
 			database.print() ;
 		} 
 		else if( option == 2 ) {
-			cout << "Enter roll number " << endl ; 
+			cout << "Enter roll number " << endl ;
 			int index ;
 			cin >> index ;
 			Student::deleteStudentEntry( database.getStudent( index - 1 ) ) ;
 			database.deleteStudent( index - 1 ) ;
 			database.print() ;
-		} 
+		}
+        else if( option == 3 ) {
+            cout << "Enter roll number " << endl ;
+            int index ;
+            cin >> index ;
+            cout << "Choose Update option" << endl ;
+            cout << "1 -> Name" << endl ;
+            int updateOption ;
+            cin >> updateOption ;
+            if ( updateOption == 1 ) {
+                cout << "Enter new name :" << endl ;
+                string name ;
+                cin >> name ;
+                Student* updateStudent = database.getStudent( index - 1 ) ;
+                database.updateName( updateStudent , name ) ;
+            }
+            database.print() ;
+        }
 		else if( option == 0 ) {
-			break ; 
+			break ;
 		}
 	}
 	
