c6b7e9215103e79890464a63e8fbe82121679d0d
shubham0204 <equipintelligence@gmail.com>
2023-10-04 07:51:19 +0530

    Added exception handling in assignment 04

--- Assignment_04/procedure.sql
+++ Assignment_04/procedure.sql
@@ -3,10 +3,16 @@ create procedure calculate_area(in radius INT)
 begin
 declare r INT;
 declare a FLOAT;
+declare msg VARCHAR(20) ; 
+declare exit handler for SQLSTATE '45000' select msg;
+set msg = 'Invalid Radius';
 set r = radius;
 if r >= 5 and r <= 9 then
     set a = 3.14 * r *r;
     insert into area(radius,area) values(r,a);
+else
+    SIGNAL SQLSTATE '45000';
+end if;
 end
 $$
 delimiter ;
