57fec469b7c2639588b87bee0ae039447e5cacf4
Shubham Panchal <panchalshubhamdev@gmail.com>
2023-11-10 19:17:02 +0530

    delete: macro processor c++ version

--- macro_processor/main.cpp
+++
@@ -1,7 +0,0 @@
-#include "macro_processor_pass_1.cpp"
-
-int main( int argc , char** argv ) {
-    MacroProcessor macroProcessor( "testcase_02.txt" ) ;
-    MacroProcessor.perform() ; 
-    return 0;
-}
