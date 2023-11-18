ca6c40ed8af1c6e271efd2c7d38bd9a13fd88ae2
Shubham Panchal <panchalshubhamdev@gmail.com>
2023-11-18 20:11:28 +0530

    update: py-page-replacement and README.md

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 * [cpp-assembler](https://github.com/shubham0204/PICT_Coursework/tree/lp-1/cpp-assembler)
 * [py-macro-processor](https://github.com/shubham0204/PICT_Coursework/tree/lp-1/py-macro-processor)
-* [cpp-page-replacement](https://github.com/shubham0204/PICT_Coursework/tree/lp-1/cpp-page-replacement)
+* [py-page-replacement](https://github.com/shubham0204/PICT_Coursework/tree/lp-1/py-page-replacement)
 * [py-scheduling-algorithms](https://github.com/shubham0204/PICT_Coursework/tree/lp-1/py-scheduling-algorithms)
 * [py-election-algorithms](https://github.com/shubham0204/PICT_Coursework/tree/lp-1/py-election-algorithms)
 * [java-multithreaded-echo-server](https://github.com/shubham0204/PICT_Coursework/tree/lp-1/java-multithreaded-echo-server)
--- py-page-replacement/assignment.py
+++ py-page-replacement/assignment.py
@@ -1,108 +1,86 @@
 from collections import deque
 
-def first_in_first_out( page_refs , maxlen=3 ):
-    memory_frames = deque( maxlen=maxlen )
-    hits = 0
+# refs -> page references
+# memory_size -> max. number of pages that can be accomodated in the memory
+# memory -> buffer in which pages are stored
 
-    for i , ref in enumerate( page_refs ):
-
-        if ref not in memory_frames:
-            if len( memory_frames ) == maxlen:
-                memory_frames.popleft()
-            memory_frames.append( ref )
-            print( "MISS " + str(list(memory_frames)) )
-        else:
-            hits += 1
-            print( "HIT  " + str(list(memory_frames)) )
-
-    return hits , len( page_refs ) - hits
-
-
-def least_recently_used( page_refs , maxlen=3 ):
-    memory_frames = []
-    hits = 0
-    ref_ages = dict( [ ( ref , 0 ) for ref in page_refs ] )
-
-    for ref in page_refs:
-
-        # Increment the age of page
-        # references in memory
-        for mem_ref in memory_frames:
-            ref_ages[ mem_ref ] += 1
-
-        if not ref in memory_frames:
+def first_in_first_out( refs: list[int] , memory_size: int ):
+    memory = deque()
+    hits: int = 0
+    for ref in refs:
+        if ref not in memory:
             # Page fault occurs
-
-            # Determine reference with maximum age
-            # in ref_ages
-            if len( memory_frames ) == maxlen:
-                lru_ref_index = 0
-                lru_ref = memory_frames[ lru_ref_index ]
-                lru_ref_time = ref_ages[ memory_frames[ lru_ref_index ] ] 
-                for i , mem_ref in enumerate(memory_frames):
-                    if ref_ages[ mem_ref ] > lru_ref_time:
-                        lru_ref = mem_ref
-                        lru_ref_time = ref_ages[ mem_ref ]
-                        lru_ref_index = i
-                memory_frames[ lru_ref_index ] = ref
-                ref_ages[ lru_ref ] = 0
-            else:
-                memory_frames.append( ref )
-                ref_ages[ ref ] = 0
-            print( "MISS " + str(ref) + " " + str(memory_frames) )
-
+            # swap out leftmost page and swap in `ref` if memory is full
+            if len( memory ) == memory_size:
+                memory.popleft()
+            memory.append( ref )
         else:
-            ref_ages[ ref ] = 0
             hits += 1
-            print( "HIT  " + str(ref) + " "  + str(memory_frames) )
+        print( ref , list(memory) ) 
+    misses: int = len( refs ) - hits
+    return hits , misses
 
-    return hits , len( page_refs ) - hits
-
-
-def optimal( page_refs , maxlen=3 ):
-    memory_frames = []
-    hits = 0
-
-    for i , ref in enumerate( page_refs ):
-
-        if not ref in memory_frames:
+def least_recently_used( refs: list[int] , memory_size: int ):
+    memory: list[int] = []
+    hits: int = 0 
+    for ref_index , ref in enumerate(refs):
+        if ref not in memory:
             # Page fault occurs
-
-            if len( memory_frames ) == maxlen:
-                max_usage_index = 0
-                max_usage = 0
-                for p in range( len( memory_frames ) ):
-                    for j in range( i + 1 , len( page_refs ) ):
-                        if page_refs[ j ] == memory_frames[ p ]:
-                            if j - i > max_usage:
-                                max_usage = j - i
-                                max_usage_index = p
-                            break
-                    else:
-                        max_usage_index = p
-                        break
-                memory_frames[ max_usage_index ] = ref
-
+            # swap out least recently accessed page and swap in `ref` if memory is full
+            if len( memory ) == memory_size:
+                access_gaps: list[int] = []
+                # Iterate through each page in memory
+                # and check which one was least recently accessed
+                for memory_index , memory_ref in enumerate( memory ):
+                    j: int = ref_index - 1
+                    while refs[j] != memory_ref and j >= 0:
+                        j -= 1
+                    access_gaps.append( ref_index-j )
+                lru_frame_index = access_gaps.index( max( access_gaps ) )
+                memory[ lru_frame_index ] = ref
             else:
-                memory_frames.append( ref )
-            print( "MISS " + str(ref) + " " + str(memory_frames) )
-
+                memory.append( ref )
         else:
             hits += 1
-            print( "HIT  " + str(ref) + " "  + str(memory_frames) )
+        print( ref , list(memory) ) 
+    misses: int = len( refs ) - hits
+    return hits , misses
 
-    return hits , len( page_refs ) - hits
-
+def optimal( refs: list[int] , memory_size: int ):
+    memory: list[int] = []
+    hits: int = 0 
+    for ref_index , ref in enumerate(refs):
+        if ref not in memory:
+            # Page fault occurs
+            # swap out page farthest away in the refs
+            if len( memory ) == memory_size:
+                access_gaps: list[int] = []
+                # Iterate through each page in memory
+                # and check which one was least recently accessed
+                for memory_index , memory_ref in enumerate( memory ):
+                    j: int = ref_index + 1
+                    while j < len( refs ) and refs[j] != memory_ref:
+                        j += 1
+                    access_gaps.append( j - (ref_index + 1) )
+                max_gap_index = access_gaps.index( max( access_gaps ) )
+                memory[ max_gap_index ] = ref
+            else:
+                memory.append( ref )
+        else:
+            hits += 1
+        print( ref , list(memory) ) 
+    misses: int = len( refs ) - hits
+    return hits , misses
 
 
 refs = [ 1 , 3 , 0 , 3 , 5 , 6 , 3 , 4 , 7 , 1 , 0 , 8 ]
-hits , misses = first_in_first_out( refs , maxlen=3 ) 
+hits , misses = first_in_first_out( refs , memory_size=3 ) 
 print( hits , misses )
 
 refs = [ 1 , 3 , 0 , 3 , 5 , 6 , 3 , 4 , 7 , 1 , 0 , 8 ]
-hits , misses = least_recently_used( refs , maxlen=3 ) 
+hits , misses = least_recently_used( refs , memory_size=3 ) 
 print( hits , misses )
 
 refs = [ 1 , 3 , 0 , 3 , 5 , 6 , 3 , 4 , 7 , 1 , 0 , 8 ]
-hits , misses = optimal( refs , maxlen=3 ) 
+hits , misses = optimal( refs , memory_size=3 ) 
 print( hits , misses )
