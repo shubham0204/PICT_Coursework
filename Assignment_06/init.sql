a4753a254e810a2e4d426bf650a2d17ed065d5a6
Shubham Panchal <panchalshubhamdev@gmail.com>
2024-02-05 20:36:44 +0530

    [new] Assignment 06 - JSP + JDBC

---
+++ Assignment_06/init.sql
@@ -0,0 +1,55 @@
+-- MariaDB dump 10.19-11.0.2-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: students_db
+-- ------------------------------------------------------
+-- Server version	11.0.2-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `students`
+--
+
+DROP TABLE IF EXISTS `students`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `students` (
+  `stud_id` int(11) NOT NULL AUTO_INCREMENT,
+  `stud_name` varchar(20) DEFAULT NULL,
+  `class` smallint(5) unsigned DEFAULT NULL,
+  `division` char(1) DEFAULT NULL,
+  `city` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`stud_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `students`
+--
+
+LOCK TABLES `students` WRITE;
+/*!40000 ALTER TABLE `students` DISABLE KEYS */;
+INSERT INTO `students` VALUES
+(1,'Shubham',10,'A','Thane');
+/*!40000 ALTER TABLE `students` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-02-05 20:35:41
---
+++ Assignment_06/students_info.jsp
@@ -0,0 +1,34 @@
+<html>
+
+    <head>
+        <title>JDBC Connectivity with JSP</title>
+    </head>
+
+    <% 
+        java.sql.Connection connection = java.sql.DriverManager.getConnection( "jdbc:mariadb://localhost:3306/students_db" , "root" , "root" ) ; 
+        java.sql.ResultSet results = connection.createStatement().executeQuery( "select * from students" ) ; 
+    %>
+
+     <table>
+        <tr>
+           <th>Student ID</th>
+           <th>Name</th>
+           <th>Class</th>
+           <th>Division</th>
+           <th>City</th>
+        </tr>
+        <% while( results.next() ) { %>
+            <tr>
+                <td><%= results.getInt( "stud_id" )%></td>
+                <td><%= results.getString( "stud_name" )%></td>
+                <td><%= results.getInt( "class" )%></td>
+                <td><%= results.getString( "division" )%></td>
+                <td><%= results.getString( "city" )%></td>
+            </tr>
+        <% } %>
+        <% connection.close() ; %>
+     </table>
+     
+    </body>
+
+</html>
