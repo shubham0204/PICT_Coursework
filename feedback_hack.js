3bb09b205c4a5ed5045862dff6c08b32e7458dfd
Shubham Panchal <panchalshubhamdev@gmail.com>
2024-05-07 19:50:42 +0530

    add feedback_hack.js

---
+++ feedback_hack.js
@@ -0,0 +1,8 @@
+const top_option = document.getElementById("seleted_answer_1")
+const proceed_button = document.getElementById("btnNext")
+const submit_button = document.getElementById("BtnSubmit")
+for( i = 0 ; i < 5 ; i++ ) {
+    top_option.onclick()
+    proceed_button.onclick()
+}
+submit_button.onclick()
