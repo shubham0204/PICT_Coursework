c28ae36a96af5af144d0248bc9a2f2a09707790e
Shubham Panchal <panchalshubhamdev@gmail.com>
2024-05-08 18:38:47 +0530

    fix prims.py

--- 03_Greedy/prims.py
+++ 03_Greedy/prims.py
@@ -12,23 +12,6 @@ class Graph:
         self.adj_list[node_2].append((node_1, weight))
         self.edges.append((node_1, node_2, weight))
 
-    def check_cycle(self, src: str, dst: str) -> bool:
-        if src not in self.adj_list or dst not in self.adj_list:
-            return False
-        queue = [src]
-        visited = [src]
-        while len(queue) != 0:
-            front = queue[0]
-            del queue[0]
-            if front == dst:
-                return True
-            neighbors = self.adj_list[front]
-            for neighbor in neighbors:
-                if neighbor[0] not in visited:
-                    visited.append(neighbor[0])
-                    queue.append(neighbor[0])
-        return False
-
     def __str__(self) -> str:
         str_repr = ""
         for key, val in self.adj_list.items():
@@ -37,29 +20,35 @@ class Graph:
 
 
 def min_spanning_tree(graph: Graph):
-    curr_node = list(graph.adj_list.keys())[0]
+    start_node = list(graph.adj_list.keys())[0]
     graph_num_nodes = len(graph.adj_list)
     tree_num_edges = 1
     min_cost = 0
-    visited: list[str] = [curr_node]
+    visited: list[str] = [start_node]
     spanning_tree: Graph = Graph()
     while tree_num_edges <= graph_num_nodes - 1:
-        min_weight_edge = min(
-            [
-                neighbor
-                for neighbor in graph.adj_list[curr_node]
-                if neighbor[0] not in visited
-            ],
-            key=lambda x: x[1],
-            default=None,
-        )
-        if min_weight_edge is None:
-            continue
-        spanning_tree.add_edge(curr_node, min_weight_edge[0], min_weight_edge[1])
-        min_cost += min_weight_edge[1]
-        curr_node = min_weight_edge[0]
+        min_weight_edge = None
+        min_weight = 1e10
+        for v in visited:
+            edge = min(
+                [
+                    neighbor
+                    for neighbor in graph.adj_list[v]
+                    if neighbor[0] not in visited
+                ],
+                key=lambda x: x[1],
+                default=None,
+            )
+            if edge is None:
+                continue
+            if edge[1] < min_weight:
+                min_weight = edge[1]
+                min_weight_edge = ( v , edge[0] , edge[1] )
+        print( f"Edge added: {min_weight_edge[0]} - {min_weight_edge[1]} , weight = {min_weight_edge[2]}")
+        spanning_tree.add_edge( min_weight_edge[0] , min_weight_edge[1] , min_weight_edge[2] )
+        min_cost += min_weight_edge[2]
         tree_num_edges += 1
-        visited.append(min_weight_edge[0])
+        visited.append(min_weight_edge[1])
     print(f"Min cost of spanning tree {min_cost}")
 
 
