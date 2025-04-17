d71af1d1cb0228e2731458263ca12267e546dfe6
Shubham <noreply+shubham@objectbox.io>
2025-04-17 07:02:56 +0530

    add assignment 02-parallel-sort-algs

---
+++ 02_Parallel_Sort_Algos/par_bubble_sort.cpp
@@ -0,0 +1,84 @@
+/*
+Parallel BubbleSort implementation using OpenMP directives
+@author shubham0204 (Shubham Panchal)
+@date   17/04/2025
+
+Usage:
+
+1. Change the value of `TEST_ARR_SIZE` that represents the size of the array to be sorted.
+
+2. Compile this code,
+
+   g++ par_bubble_sort.cpp -fopenmp -o main
+
+3. Use the `time` utility in Linux to measure the time taken by the executable to run,
+
+   time ./main par
+   time ./main seq
+
+The differences in the `real` or wall-time should be significant.
+*/
+#include <iostream>
+#include <omp.h>
+#include <utility>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
+
+#define TEST_ARR_SIZE 100000
+
+void sequentialBubbleSort(std::vector<int>& arr) {
+    for (int i = 0; i < arr.size(); i++) {
+        for (int j = 0; j < arr.size() - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+};
+
+void parallelBubbleSort(std::vector<int>& arr) {
+    bool sorted = false;
+    while (!sorted) {
+        bool localSorted = true;
+        // spawn a new thread for each iteration of the loop
+        #pragma omp parallel for reduction(&&: localSorted)
+        for (int i = 0; i < arr.size(); i += 2) {
+            if (arr[i] > arr[i+1]) {
+                std::swap(arr[i], arr[i+1]);
+                localSorted = false;
+            }
+        }
+        #pragma omp parallel for reduction(&&: localSorted)
+        for (int i = 1; i < arr.size(); i += 2) {
+            if (arr[i] > arr[i+1]) {
+                std::swap(arr[i], arr[i+1]);
+                localSorted = false;
+            }
+        }
+        sorted = localSorted;
+    }
+}
+
+std::vector<int> generateRandomArray(int size) {
+    std::vector<int> arr(size);
+    for (int i = 0; i < size; i++) {
+        arr[i] = rand() % 100 + 1;
+    }
+    return arr;
+};
+
+int main(int argc, char* argv[]) {
+    srand(time(nullptr));
+    std::vector<int> nums = generateRandomArray(TEST_ARR_SIZE);
+    if (std::string(argv[1]) == "seq") {
+        std::cout << "executing sequential bubble-sort ..." << '\n';
+        sequentialBubbleSort(nums);
+    } else {
+        std::cout << "executing parallel bubble-sort ..." << '\n';
+        parallelBubbleSort(nums);
+    }
+    return 0;
+}
---
+++ 02_Parallel_Sort_Algos/par_merge_sort.cpp
@@ -0,0 +1,121 @@
+/*
+Parallel MergeSort implementation using OpenMP directives
+@author shubham0204 (Shubham Panchal)
+@date   17/04/2025
+
+Usage:
+
+1. Set `N_CPU_CORES` to the number of available CPU cores. Use `lscpu` in the terminal
+   to get the number of CPU cores quickly on most Linux distros. Also, you may change the value
+   of `TEST_ARR_SIZE` that represents the size of the array to be sorted.
+
+2. Compile this code,
+
+   g++ par_merge_sort.cpp -fopenmp -o main
+
+3. Use the `time` utility in Linux to measure the time taken by the executable to run,
+
+   time ./main par
+   time ./main seq
+
+The differences in the `real` or wall-time should be significant.
+*/
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <vector>
+
+#define TEST_ARR_SIZE 100000
+#define N_CPU_CORES 4
+
+void merge(std::vector<int>& arr, int low, int mid, int high) {
+    // create two arrays:
+    // arr1 = arr[low:mid]
+    // arr2 = arr[mid:high+1]
+    int n1 = mid - low + 1;
+    int n2 = high - mid;
+    std::vector<int> arr1(n1);
+    std::vector<int> arr2(n2);
+    for (int i = 0; i < n1; i++) {
+        arr1[i] = arr[low + i];
+    }
+    for (int i = 0; i < n2; i++) {
+        arr2[i] = arr[mid + i];
+    }
+
+    // merge two sorted arrays, arr1 and arr2
+    int i = 0;
+    int j = 0;
+    int k = low;
+    while (i < n1 && j < n2) {
+        if (arr1[i] < arr2[j]) {
+            arr[k] = arr1[i];
+            i++;
+        } else {
+            arr[k] = arr2[j];
+            j++;
+        }
+        k++;
+    }
+    while (i < n1) {
+        arr[k] = arr1[i];
+        i++;
+        k++;
+    }
+    while (j < n2) {
+        arr[k] = arr2[j];
+        j++;
+        k++;
+    }
+}
+
+void sequentialMergeSort(std::vector<int>& arr, int low, int high) {
+    if (low < high) {
+        int mid = (low + high) / 2;
+        sequentialMergeSort(arr, low, mid);
+        sequentialMergeSort(arr, mid + 1, high);
+        merge(arr, low, mid, high);
+    }
+}
+
+// merge sort with parallel recursion
+// https://en.wikipedia.org/wiki/Merge_sort#Merge_sort_with_parallel_recursion
+void parallelMergeSort(std::vector<int>& arr, int low, int high, int depth = 0) {
+    if (low < high) {
+        int mid = (low + high) / 2;
+        if (depth < N_CPU_CORES) {
+#pragma omp parallel sections
+            {
+#pragma omp section
+                { parallelMergeSort(arr, low, mid, depth + 1); }
+#pragma omp section
+                { parallelMergeSort(arr, mid + 1, high, depth + 1); }
+            }
+        } else {
+            parallelMergeSort(arr, low, mid, depth + 1);
+            parallelMergeSort(arr, mid + 1, high, depth + 1);
+        }
+        merge(arr, low, mid, high);
+    }
+}
+
+std::vector<int> generateRandomArray(int size) {
+    std::vector<int> arr(size);
+    for (int i = 0; i < size; i++) {
+        arr[i] = rand() % 100 + 1;
+    }
+    return arr;
+};
+
+int main(int argc, char* argv[]) {
+    srand(time(nullptr));
+    std::vector<int> nums = generateRandomArray(TEST_ARR_SIZE);
+    if (std::string(argv[1]) == "seq") {
+        std::cout << "executing sequential merge-sort ..." << '\n';
+        sequentialMergeSort(nums, 0, nums.size());
+    } else {
+        std::cout << "executing parallel merge-sort ..." << '\n';
+        parallelMergeSort(nums, 0, nums.size());
+    }
+    return 0;
+}
